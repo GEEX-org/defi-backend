e2cab45c7bbe6d34b346ca749e57b3badb8cec1b
KUNAL KUMAR <kunaldevspro@gmail.com>
2025-02-28 05:20:37 +0530

    vm: push for AWS EC2  hosting

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const JWT_SECRET = "i_love_u_kunal2134";
+export const WORKER_JWT_SECRET= JWT_SECRET+"WORKER";
+export const TOTAL_DECIMALS = 1000_000;
---
+++ src/privateKey.ts
@@ -0,0 +1,3 @@
+
+//export const privateKey = "mKq7hg7oZ7QBNwdfp4WWhwM9eZz53ieDJ67YXscFbm5MEDcBKgknh8HxXozGYE2VMXCVMij5uM2FziduyWK1ANk";
+export const privateKey="4UYipgyocBUWefyVJmpSNSiuAtCconovkXshJRsmKr781gPeJBh6dKtJY3e4YKmDBY4pZ5xTCcY63J7gtFoPt79X"
