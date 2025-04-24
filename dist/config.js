c5b74ab449334a6083efa38c0bb4e4d0195945ad
KUNAL KUMAR <kunaldevspro@gmail.com>
2025-04-24 13:01:44 +0530

    chore: add dist to the root

---
+++ dist/config.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TOTAL_DECIMALS = exports.WORKER_JWT_SECRET = exports.JWT_SECRET = void 0;
+exports.JWT_SECRET = "i_love_u_kunal2134";
+exports.WORKER_JWT_SECRET = exports.JWT_SECRET + "WORKER";
+exports.TOTAL_DECIMALS = 1000000;
---
+++ dist/privateKey.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.privateKey = void 0;
+//export const privateKey = "mKq7hg7oZ7QBNwdfp4WWhwM9eZz53ieDJ67YXscFbm5MEDcBKgknh8HxXozGYE2VMXCVMij5uM2FziduyWK1ANk";
+exports.privateKey = "4UYipgyocBUWefyVJmpSNSiuAtCconovkXshJRsmKr781gPeJBh6dKtJY3e4YKmDBY4pZ5xTCcY63J7gtFoPt79X";
